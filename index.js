be165563d6c6b97e5ba53ed1b7b5749bb07b72d2
VikramTak08 <vikramtak72@gmail.com>
2025-06-29 10:30:52 +0530

    near to geowithin changed

--- index.js
+++ index.js
@@ -152,16 +152,29 @@ app.post("/match-drivers", async (req, res) => {
   console.log("Received rider data:", req.body);
   try {
     // Step 1: Find drivers whose routes are near the rider's pickup location
+
+    const radiusInMeters = 2000;
+const earthRadiusInMeters = 6378137;
     const driversNearPickup = await Driver.find({
+
+
       waypoints: {
-        $near: {
-          $geometry: {
-            type: "Point",
-            coordinates: pickupCoords,
-          },
-          $maxDistance: 2000,
-        },
-      },
+    $geoWithin: {
+      $centerSphere: [
+        pickupCoords, // [lng, lat]
+        radiusInMeters / earthRadiusInMeters
+      ]
+    }
+  }
+      // waypoints: {
+      //   $near: {
+      //     $geometry: {
+      //       type: "Point",
+      //       coordinates: pickupCoords,
+      //     },
+      //     $maxDistance: 2000,
+      //   },
+      // },
     });
 
     // Step 2: Filter drivers further by checking if their routes are also near the destination location
